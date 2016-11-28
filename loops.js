c60529c19873625aa7f7007ed971f73533c7d0e0
GwenRaq <gwen.raq@gmail.com>
2016-11-28 08:45:02 -1000

    11/28

---
+++ loops.js
@@ -0,0 +1,26 @@
+//Mommy
+for (var count = 0; count <= 100; count++){
+	console.log("There are" + " "+ count +" "+ "carrots.");
+}
+
+//NoahArc
+for (var animalCount = 0; animalCount < 5; animalCount++){
+	console.log("There are" +" "+ animalCount+" "+ "many animals in Noahs Arc.");
+}
+
+//HipHopHorray
+var partyAnimals = ["Bunny","Zebra","Lion","Elephant","Monkey"]
+for (var partyAnimals = 0; partyAnimals < 5; animalCount++){
+	console.log(partyAnimals);
+}
+
+//Merlin
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+var hero = {
+	wizard : "Merlin",
+	weapon : "Excalibur",
+	power : 2
+}
+function (one){
+
+}
