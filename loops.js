e9929e784076cc101a5c658bdea7e2740eb65598
GwenRaq <gwen.raq@gmail.com>
2016-11-30 08:59:41 -1000

    11/30

--- loops.js
+++ loops.js
@@ -4,23 +4,29 @@ for (var count = 0; count <= 100; count++){
 }
 
 //NoahArc
-for (var animalCount = 0; animalCount < 5; animalCount++){
+for (var animalCount = 0; animalCount < 6; animalCount++){
 	console.log("There are" +" "+ animalCount+" "+ "many animals in Noahs Arc.");
 }
 
 //HipHopHorray
-var partyAnimals = ["Bunny","Zebra","Lion","Elephant","Monkey"]
-for (var partyAnimals = 0; partyAnimals < 5; animalCount++){
-	console.log(partyAnimals);
+var partyAnimals = ["Bunny","Zebra","Lion","Giraffe","Monkey"]
+for (a = 0; a < 6; a++){
+	console.log("I have a" + " "+ partyAnimals[a]);
 }
 
 //Merlin
 var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
 var hero = {
 	wizard : "Merlin",
-	weapon : "Excalibur",
+	weapon : weaponChest[2],
 	power : 2
 }
-function (one){
-
+function War(weapon){
+	if(hero.wizard=="Merlin" && hero.weapon=="Excalibur"){
+		console.log(hero.wizard+" "+ "shall slay the dragon with the"+" "+ hero.weapon);
+	}else if(weaponChest[1]){
+		console.log(hero.wizard+" "+"is ready for war with the"+" "+ hero.weapon);
+	}
 }
+
+War(hero.weapon);
